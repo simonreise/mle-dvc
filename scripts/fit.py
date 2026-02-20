ef769051ad00f64790ae61497f449b8b5e0d807e
Mikhail <moskovchenkomike@gmail.com>
2026-02-20 12:33:26 +0000

    Run a sklearn-only pipeline

--- scripts/fit.py
+++ scripts/fit.py
@@ -3,9 +3,8 @@
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
 import yaml
 import os
 import joblib
@@ -30,14 +29,14 @@ def fit_model():
     preprocessor = ColumnTransformer(
         [
             ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(drop='if_binary'), other_cat_features.columns.tolist()),
             ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    model = CatBoostClassifier(auto_class_weights='Balanced')
+    model = LogisticRegression(C=1, penalty='l2')
 
     pipeline = Pipeline(
         [
